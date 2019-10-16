13e85ea18d022bf697979774fda2914e707870d7
chaithanya008 <krishna.chaithanya.8@gmail.com>
2019-10-17 01:34:04 +0200

    added logic in next function

--- src/app/next.service.ts
+++ src/app/next.service.ts
@@ -1,9 +1,5 @@
 import { Injectable, Component } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-
 @Component({
   selector: 'nextbutton',
   template: ` {{ nextfunc() }} `
@@ -14,5 +10,21 @@ export class NextService {
   constructor() { }
 
   nextfunc() {
+    next() {
+      let step1, step2, step3, step4, step5, step6, step7;
+      let step = step1;
+      switch (step) {
+        case step1:
+          step = 'step2';
+          step1 = document.getElementById('step1');
+          step1.classList.remove('is-active');
+          step1.classList.add('is-complete');
+
+        case step2:
+          step = 'step3';
+            step2.classList.remove("is-active");
+            step2.classList.add("is-complete");
+            step3.classList.add("is-active");
+      }
   }
 }
